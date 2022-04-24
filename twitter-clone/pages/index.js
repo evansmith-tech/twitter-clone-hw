36deb30647b2976691d22be0948b324095ca8d92
Evan Smith <evan.c.smith@ttu.edu>
2022-04-23 19:36:35 -0500

    Rough Alignment of UI
    some minor UI fixes still needed, such as proper alignment of the side nav

--- twitter-clone/pages/index.js
+++ twitter-clone/pages/index.js
@@ -1,6 +1,9 @@
+import { Box, Heading, HStack, Text, VStack, Flex, Center } from '@chakra-ui/react';
 import { ClassNames } from '@emotion/react';
 import Head from 'next/head'
 import Image from 'next/image'
+import SideNav from '../components/sideNav';
+import SignIn from '../components/signIn';
 import Tweet from '../components/tweet';
 import TweetFeed from '../components/tweetFeed';
 // import styles from '../styles/Home.module.css'
@@ -9,12 +12,32 @@ import TweetFeed from '../components/tweetFeed';
 export default function Home(props) {
   console.log(props);
   return (
-<html>
+    <html>
       <head>
         <title>Twitter Clone</title>
       </head>
       <body>
-        <TweetFeed tweets={props.timeline.tweets}></TweetFeed>
+        <VStack width="100%" alignContent="start">
+          {/* placeholder nav bar */}
+          
+            <HStack width="100%" bgColor="blue.400" alignContent={"space-between"}>
+
+            <Heading padding={".2em .3em"} textColor="white">Twitter Clone</Heading>
+          <SignIn />
+          </HStack>
+
+          {/* Actual UI */}
+
+
+          <HStack align={"space-between"} width="100%">
+            <SideNav />
+
+                <TweetFeed heading="Following Feed" tweets={props.timeline.tweets}></TweetFeed>
+                <TweetFeed heading="Global Feed" tweets={props.timeline.tweets}></TweetFeed>
+           
+          </HStack>
+
+        </VStack>
       </body>
     </html>
   );
