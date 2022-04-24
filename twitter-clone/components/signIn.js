13c4948a344c0fa5e9971d83ea575e9ff0f188de
Evan Smith <evan.c.smith@ttu.edu>
2022-04-23 19:35:43 -0500

    mock sign up page, not working

---
+++ twitter-clone/components/signIn.js
@@ -0,0 +1,12 @@
+import { Button, HStack, Input } from "@chakra-ui/react";
+
+export default function SignIn(props) {
+    return (
+        <HStack>
+            <Input placeholder="email" type="email"></Input>
+            <Input placeholder="passwd" type="password"></Input>
+            <Button colorScheme="twitter">Sign In</Button>
+        </HStack>
+
+    );
+}
