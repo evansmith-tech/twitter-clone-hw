8191012d8babf3530201c9630b4c49e809d3587c
Evan Smith <evan.c.smith@ttu.edu>
2022-04-23 19:34:41 -0500

    Added needed content

---
+++ twitter-clone/components/sideNav.js
@@ -0,0 +1,17 @@
+import { ChevronRightIcon } from "@chakra-ui/icons";
+import { HStack, Icon, Text, VStack } from "@chakra-ui/react";
+
+export default function SideNav(props) {
+    return (<VStack alignContent="start">
+        <NavItem icon={<ChevronRightIcon/>} heading="Home"/>
+        <NavItem icon={<ChevronRightIcon/>} heading="Explore"/>
+        <NavItem icon={<ChevronRightIcon/>} heading="Profile"/>
+    </VStack>);
+}
+
+function NavItem(props) {
+    return(<HStack alignContent="space-evenly">
+        {props.icon}
+        <Text>{props.heading}</Text>
+    </HStack>)
+}
