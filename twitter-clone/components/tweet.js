c5dcd3ddc4a2bd0bebe867bdd47b0e11c1241262
Evan Smith <evan.c.smith@ttu.edu>
2022-04-27 14:34:26 -0500

    Hooked up to backend functionality

--- twitter-clone/components/tweet.js
+++ twitter-clone/components/tweet.js
@@ -1,17 +1,44 @@
 import { Box, Text, Heading, Image, VStack, HStack, IconButton, Avatar, Flex, Icon } from "@chakra-ui/react"
 import { ChatIcon, RepeatIcon, CheckIcon, WarningTwoIcon } from '@chakra-ui/icons'
-
+import React, {useState} from 'react';
 export default function Tweet(props) {
 
-    function comment() {
+    const [isLiked, change_like_btn_state] = useState(false)
+    const [isRetweeted, change_retweet_btn_state] = useState(false)
+    // const [like_count, like_count_state] = useState(props.likes)
+
+    const comment = function comment() {
         // call the route in the comment api
         console.log("hi")
     }
-    function retweet() {
-
+    const retweet = async function retweet() {        
+        //! this doesn't unretweet the likes serverside
+        await fetch ("http://localhost:3000/api/tweets/retweet", {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                  postId: props.postId,
+                  userId: 123 // todo AUTH comeback and swap this for the user ID determined at runtime
+              })
+        });
+        change_retweet_btn_state(!isRetweeted); // whatever the curernt state is, we flip it
     }
-    function like() {
-
+    const like = async function like() {
+        //! this doesn't decrement the likes serverside
+        await fetch ("http://localhost:3000/api/tweets/incrementLikes", {
+            method: "POST",
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({
+                  postId: props.postId
+              })
+        });
+        change_like_btn_state(!isLiked); // whatever the current state is, we flip it
     }
 
     // const props = {
@@ -35,9 +62,9 @@ export default function Tweet(props) {
                 <HStack alignContent="space-between">
                     {/* <Flex> */}
 
-                    <IconButton variant={"ghost"} onClick={comment()} aria-label='Comment' icon={<ChatIcon />} />
-                    <IconButton variant={"ghost"} onClick={retweet()} aria-label='Retweet' icon={<RepeatIcon />} />
-                    <IconButton variant={"ghost"} onClick={like()} aria-label='Like' icon={<CheckIcon />} />
+                    <IconButton colorScheme={false ? "twitter" : ""} variant={"ghost"} onClick={comment} aria-label='Comment' icon={<ChatIcon />} />
+                    <IconButton colorScheme={isRetweeted ? "twitter" : ""} variant={"ghost"} onClick={retweet} aria-label='Retweet' icon={<RepeatIcon />} />
+                    <IconButton colorScheme={isLiked ? "twitter" : ""} variant={"ghost"} onClick={like} aria-label='Like' icon={<CheckIcon />} />
                     {false ? <IconButton colorScheme={"yellow"} variant={'solid'} aria-label='flag' icon={<WarningTwoIcon />}></IconButton> : <Box />}
                     {/* </Flex> */}
                 </HStack>
