e4868ad3f2f623f6466bd0e5943439b0384503d5
Evan Smith <evan.c.smith@ttu.edu>
2022-04-26 23:28:15 -0500

    Changed Icon types and prettifyied

--- twitter-clone/components/tweet.js
+++ twitter-clone/components/tweet.js
@@ -1,7 +1,18 @@
-import { Box, Text, Heading, Image, VStack, HStack, IconButton, Avatar,Flex } from "@chakra-ui/react"
-import { ChatIcon, RepeatIcon, CheckIcon } from '@chakra-ui/icons'
+import { Box, Text, Heading, Image, VStack, HStack, IconButton, Avatar, Flex, Icon } from "@chakra-ui/react"
+import { ChatIcon, RepeatIcon, CheckIcon, WarningTwoIcon } from '@chakra-ui/icons'
 
 export default function Tweet(props) {
+
+    function comment() {
+        // call the route in the comment api
+        console.log("hi")
+    }
+    function retweet() {
+
+    }
+    function like() {
+
+    }
 
     // const props = {
     //     name: "Dan Abrahmov",
@@ -12,25 +23,26 @@ export default function Tweet(props) {
     return (
         <Box boxShadow='xs' p='6' rounded='md' bg='white'>
 
-        <VStack alignItems="center">
-            
-        <HStack>
-            <Avatar name={props.name} src={props.avatar_link} />
-            <VStack alignItems="start">
-                <Text>{props.name} {props.timestamp}</Text>
-                <Text>{props.tweet}</Text>
-            </VStack>
-        </HStack>
-                <HStack alignContent="space-between">   
-                {/* <Flex> */}
+            <VStack alignItems="center">
 
-                    <IconButton aria-label='Comment' icon={<ChatIcon />} />
-                    <IconButton aria-label='Retweet' icon={<RepeatIcon />} />
-                    <IconButton aria-label='Upvote' icon={<CheckIcon />} />
-                {/* </Flex> */}
+                <HStack>
+                    <Avatar name={props.name} src={props.avatar_link} />
+                    <VStack alignItems="start">
+                        <Text>{props.name} {props.timestamp}</Text>
+                        <Text>{props.tweet}</Text>
+                    </VStack>
+                </HStack>
+                <HStack alignContent="space-between">
+                    {/* <Flex> */}
+
+                    <IconButton variant={"ghost"} onClick={comment()} aria-label='Comment' icon={<ChatIcon />} />
+                    <IconButton variant={"ghost"} onClick={retweet()} aria-label='Retweet' icon={<RepeatIcon />} />
+                    <IconButton variant={"ghost"} onClick={like()} aria-label='Like' icon={<CheckIcon />} />
+                    {false ? <IconButton colorScheme={"yellow"} variant={'solid'} aria-label='flag' icon={<WarningTwoIcon />}></IconButton> : <Box />}
+                    {/* </Flex> */}
                 </HStack>
 
-        </VStack>
+            </VStack>
         </Box>
     );
 }
