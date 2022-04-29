c01157cba22f0e27c4afbeb8039baa349aa9088b
Evan Smith <evan.c.smith@ttu.edu>
2022-04-28 22:49:48 -0500

    removed explore option

--- twitter-clone/components/sideNav.js
+++ twitter-clone/components/sideNav.js
@@ -4,7 +4,7 @@ import { HStack, Icon, Text, VStack } from "@chakra-ui/react";
 export default function SideNav(props) {
     return (<VStack >
         <NavItem icon={<ChevronRightIcon/>} heading="Home"/>
-        <NavItem icon={<ChevronRightIcon/>} heading="Explore"/>
+        {/* <NavItem icon={<ChevronRightIcon/>} heading="Explore"/> */}
         <NavItem icon={<ChevronRightIcon/>} heading="Profile"/>
     </VStack>);
 }
