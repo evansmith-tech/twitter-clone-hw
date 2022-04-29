d4bea9368a72f94937420f68e3ba19da63e428bf
Evan Smith <evan.c.smith@ttu.edu>
2022-04-28 22:50:44 -0500

    removed sign in component (now just hard coded)

--- twitter-clone/components/signIn.js
+++ twitter-clone/components/signIn.js
@@ -2,11 +2,7 @@ import { Button, HStack, Input } from "@chakra-ui/react";
 
 export default function SignIn(props) {
     return (
-        <HStack>
-            <Input placeholder="email" type="email"></Input>
-            {/* <Input placeholder="passwd" type="password"></Input> */}
-            <Button colorScheme="twitter">Sign In</Button>
-        </HStack>
+    <></>
 
     );
 }
