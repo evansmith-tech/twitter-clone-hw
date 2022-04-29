11766b83bf9a3751904399681859d4a0c01a3d86
Alonso Montelongo <alonsomontelongo88@gmail.com>
2022-04-29 12:44:45 -0500

    this is the delete comment api

---
+++ twitter-clone/pages/api/tweets/deleteComment.js
@@ -0,0 +1,22 @@
+const mysql = require("mysql");
+const db = mysql.createConnection({
+    user: "root",
+    host: "localhost",
+    password: "password",
+    port:3306,
+    database: "TwitterClone",
+})
+
+
+// POST request
+export default function handler(req,res) {
+    const CommentId = 2;//req.body.CommentId
+    db.query("DELETE From comment where CommentId=?;",[CommentId], (err, result) => {
+        if (err) {
+            console.log(err);
+        } else {
+            res.send("Success");
+            console.log("Success")
+        }
+    });
+}
